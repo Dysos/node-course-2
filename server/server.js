c4f086a8c2afc603b5e30d65c3a0301ba414eff5
Victor Gro <victor@victorgro.com>
2018-10-09 21:04:09 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {ObjectID} = require("mongodb");
 
 const {mongoose} = require("./db/mongoose");
 const {User} = require("./models/user");
@@ -27,6 +28,39 @@ app.get("/todos", (req, res) => {
         res.status(400).send(e);
     });
 });
+
+app.get("/todos/:id", (req, res) => {
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        res.status(404).send("Invalid ID");
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send("ID not found");
+        } else {    
+            res.status(200).send({
+                todo
+            });
+        }
+        console.log("yeee");
+    }).catch(() => {
+        res.status(400).send("Invalid req");
+    });
+});
+
+
+
+    //valid id using isvalid
+    //404, send back emtpy send
+
+    //find by id
+        //succes
+            //if todo - send it back
+            //else sned back 404 with empty body
+        //error
+            //400 - invalid req
 
 
 app.listen(3000, () => {
